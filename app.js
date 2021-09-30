8215a91563f2edb60b5e3edfd6addd85280d9f45
Nada Yumna <naddyyum@gmail.com>
2021-09-30 21:35:05 +0700

    adding js

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function index() {
+    let app = document.getElementById('app'); 
+    let h3 = document.createElement('h3');
+    h3.innerHTML = 'Prayer Times';
+
+    app.append(h3);
+}());
