0649b1a909143bdadf95c857013b2aac2a5dfc27
Nada Yumna <naddyyum@gmail.com>
2021-09-30 21:51:41 +0700

    geolocation

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-'use strict';
-
-(function index() {
+function index() {
     let app = document.getElementById('app'); 
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
 
     app.append(h3);
-}());
+
+    userLocation();
+}
+
+index();
+
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('posisi tidak dapat diakses');
+}
+
+function userLocation() {
+    if(!navigator.geolocation){
+        alert('Geolocation tidak didukung di browser anda');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
