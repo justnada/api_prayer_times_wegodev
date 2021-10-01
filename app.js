d4a545768a2fd843ede1f66f12f456da2aff1aa9
Nada Yumna <naddyyum@gmail.com>
2021-10-01 09:39:48 +0700

    create table

--- app.js
+++ app.js
@@ -1,37 +1,69 @@
 function prayerTimes(latitude, longitude) {
+
     fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
     .then(response => response.json())
     .then(function(response){
+
         let date = new Date().getDate() - 1;
-        console.log(date);
-        console.log(response.data[date]);
+        let data = response.data[0].timings;
+
+        let app   = document.getElementById('app');
+        let table = document.createElement('table');
+        let tBody = document.createElement('tbody');
+
+        for(i in data){
+            let row  = tBody.insertRow();
+            let name = row.insertCell(0);
+            let time = row.insertCell(1);
+
+            name.innerHTML = i;
+            time.innerHTML = data[i];
+
+            tBody.appendChild(row);
+        }
+
+        console.log(response.data);
+
+        table.appendChild(tBody);
+        app.appendChild(table);
+
     });
+
 }
 
-function success(position){
+function success(position) { 
+
     prayerTimes(position.coords.latitude, position.coords.longitude);
+
 }
 
-function error(){
+function error() {
+
     alert('posisi tidak dapat diakses');
+
 }
 
 function userLocation() {
-    if(!navigator.geolocation){
+
+    if(!navigator.geolocation) {
         alert('Geolocation tidak didukung di browser anda');
-    } else {
+    } 
+    else {
         navigator.geolocation.getCurrentPosition(success, error);
     }
+
 }
 
 function index() {
-    let app = document.getElementById('app'); 
-    let h3 = document.createElement('h3');
-    h3.innerHTML = 'Prayer Times';
+
+    let app         = document.getElementById('app'); 
+    let h3          = document.createElement('h3');
+    h3.innerHTML    = 'Prayer Times';
 
     app.append(h3);
 
     userLocation();
+
 }
 
 index();
