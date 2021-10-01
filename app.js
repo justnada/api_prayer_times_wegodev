b093414f63390a7c3324b6ec59e771f44f8bb038
Nada Yumna <naddyyum@gmail.com>
2021-10-01 10:30:15 +0700

    set default geolocation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function prayerTimes(latitude, longitude) {
 
-    fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=4')
+    fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=11')
     .then(response => response.json())
     .then(function(response){
 
@@ -32,30 +32,24 @@ function prayerTimes(latitude, longitude) {
 }
 
 function success(position) { 
-
     prayerTimes(position.coords.latitude, position.coords.longitude);
-
 }
 
 function error() {
-
-    alert('posisi tidak dapat diakses');
-
+    // set default geolocation to Jakarta
+    prayerTimes('-6.200000', '106.816666');
 }
 
 function userLocation() {
-
     if(!navigator.geolocation) {
         alert('Geolocation tidak didukung di browser anda');
     } 
     else {
         navigator.geolocation.getCurrentPosition(success, error);
     }
-
 }
 
 function index() {
-
     let app         = document.getElementById('app'); 
     let h3          = document.createElement('h3');
     h3.innerHTML    = 'Prayer Times';
@@ -63,7 +57,6 @@ function index() {
     app.append(h3);
 
     userLocation();
-
 }
 
 index();
